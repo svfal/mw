dc45af2e76114ca0a7145f40732f4aeba16ed3cd
svfal <s.falkenhain@aol.de>
2022-04-26 20:31:56 +0200

    Next try

--- mw-theme/header.php
+++ mw-theme/header.php
@@ -11,16 +11,16 @@
 
 <body>
 
-
+    <div class="rectangle_green rectangle"></div>
+    <div class="rectangle_yellow rectangle"></div> 
+    <div class="rectangle_red rectangle"></div> 
+    <div class="rectangle_dark_blue rectangle"></div> 
+    <div class="rectangle_light_blue rectangle"></div> 
 
     <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
         <div class="container">
             <!-- Colored Boxes -->
-            <div class="rectangle_green rectangle"></div>
-            <div class="rectangle_yellow rectangle"></div> 
-            <div class="rectangle_red rectangle"></div> 
-            <div class="rectangle_dark_blue rectangle"></div> 
-            <div class="rectangle_light_blue rectangle"></div> 
+
             <!-- Brand and toggle get grouped for better mobile display -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
                 <span class="navbar-toggler-icon"></span>
