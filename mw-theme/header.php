4b5b252c108aa5925697c70ae42b38e05c53a6e3
Carl Isaac <carl.isaac@outlook.de>
2022-04-28 18:47:30 +0200

    Updated col-md

--- mw-theme/header.php
+++ mw-theme/header.php
@@ -30,7 +30,7 @@
 
         <div class="row">
 
-            <div class="col-md-12">
+            <div class="col-md-9">
 
                 <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
                     
