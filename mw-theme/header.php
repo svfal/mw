50186419b1e18fa7d9be1f09579e89389104e51e
Carl Isaac <carl.isaac@outlook.de>
2022-04-28 18:37:16 +0200

    Another alignment attempt

--- mw-theme/header.php
+++ mw-theme/header.php
@@ -15,7 +15,7 @@
 
         <div class="row">
 
-            <div class="col-md-10">
+            <div class="col-md-12">
 
                 <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
                     
