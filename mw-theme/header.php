3ac5610c0c04c5e4432f3cdd8012f1f62b9f8117
Carl Isaac <carl.isaac@outlook.de>
2022-04-28 18:23:38 +0200

    Changed margin of light blue rect. for alignment

--- mw-theme/header.php
+++ mw-theme/header.php
@@ -58,7 +58,7 @@
 
         <div class="row">
 
-            <div class="col-md-10">
+            <div class="col-md-8">
             
                 <!-- Colored Boxes -->
                 <div class="rectangle_green rectangle"></div>
