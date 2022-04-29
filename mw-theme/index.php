55e1127b7b392315089db8f2873c2b1fc8e9984e
Arndik <104499187+Arndik@users.noreply.github.com>
2022-04-29 17:04:13 +0200

    glaub ein div zuviel

--- mw-theme/index.php
+++ mw-theme/index.php
@@ -7,10 +7,10 @@
      		<img src= "<?php echo get_stylesheet_directory_uri().'/img/mw-start-2018.jpg'; ?>">
      	</p>
      	<p>
-        	<div>
-     			<h3>Musikwerk verbindet</h3>
-				Wir sind das Musikwerk Stuttgart. Wir haben Spa&szlig; daran, gemeinsam tolle Konzerte und anspruchsvolle Projekte auf die Beine zu stellen, die uns und andere ber&uuml;hren, begeistern oder einfach gut unterhalten. Im Musikwerk kommen ganz verschiedene Leute zusammen, verbunden durch die Freude am gemeinsamen Singen.</p> <p>So vielf&auml;ltig wir selbst sind, so vielf&auml;ltig sind unsere Ensembles.
-     		</div>
+        	
+     	<h3>Musikwerk verbindet</h3>
+		Wir sind das Musikwerk Stuttgart. Wir haben Spa&szlig; daran, gemeinsam tolle Konzerte und anspruchsvolle Projekte auf die Beine zu stellen, die uns und andere ber&uuml;hren, begeistern oder einfach gut unterhalten. Im Musikwerk kommen ganz verschiedene Leute zusammen, verbunden durch die Freude am gemeinsamen Singen.</p> <p>So vielf&auml;ltig wir selbst sind, so vielf&auml;ltig sind unsere Ensembles.
+     		
         </p>
      </div>
  <?php get_sidebar(); ?>
