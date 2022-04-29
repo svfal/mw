25747e17d3163d054da47a6146d5f7f2101458c1
Arndik <104499187+Arndik@users.noreply.github.com>
2022-04-29 18:05:57 +0200

    Update index.php

--- mw-theme/index.php
+++ mw-theme/index.php
@@ -1,18 +1,17 @@
  <?php get_header(); ?>
  
      
-     <div id="content" class="col-lg-10 col-sm-10 col-md-10 col-xs-10">    
-     	
-     	<p>
-     		<img src= "<?php echo get_stylesheet_directory_uri().'/img/mw-start-2018.jpg'; ?>">
-     	</p>
-     	<p>
-        	
-     	<h3>Musikwerk verbindet</h3>
-		Wir sind das Musikwerk Stuttgart. Wir haben Spa&szlig; daran, gemeinsam tolle Konzerte und anspruchsvolle Projekte auf die Beine zu stellen, die uns und andere ber&uuml;hren, begeistern oder einfach gut unterhalten. Im Musikwerk kommen ganz verschiedene Leute zusammen, verbunden durch die Freude am gemeinsamen Singen.</p> <p>So vielf&auml;ltig wir selbst sind, so vielf&auml;ltig sind unsere Ensembles.
-     		
-        </p>
-     </div>
+     <div id="content" >  
+		 <div class="container">
+ 			<div class="row col-lg-10 col-sm-10 col-md-10 col-xs-10">
+     	     		<img src= "<?php echo get_stylesheet_directory_uri().'/img/mw-start-2018.jpg'; ?>">
+			</div>
+      		<div class="row col-lg-10 col-sm-10 col-md-10 col-xs-10">	  	
+     			<h3>Musikwerk verbindet</h3>
+				Wir sind das Musikwerk Stuttgart. Wir haben Spa&szlig; daran, gemeinsam tolle Konzerte und anspruchsvolle Projekte auf die Beine zu stellen, die uns und andere ber&uuml;hren, begeistern oder einfach gut unterhalten. Im Musikwerk kommen ganz verschiedene Leute zusammen, verbunden durch die Freude am gemeinsamen Singen.</p> <p>So vielf&auml;ltig wir selbst sind, so vielf&auml;ltig sind unsere Ensembles.
+			</div>
+     	</div>
+	</div>
  <?php get_sidebar(); ?>
  
 
