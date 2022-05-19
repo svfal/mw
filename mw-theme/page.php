dc6892e3d74ef33e757b42c2ee1ef902e2ac4028
Arndik <104499187+Arndik@users.noreply.github.com>
2022-05-19 14:25:45 +0200

    css-Anpassungen von jetziger Seite

--- mw-theme/page.php
+++ mw-theme/page.php
@@ -1,15 +1,16 @@
  <?php get_header(); ?>
- <div id="main" class="row">
+ <div id="content" >  
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 mt-1">	  	
+     				    <h1> 
+     						<? the_title(); ?>     
+     					</h1>
+    					<? the_content(); ?>    
      
-         
- 	<div id="content" class="col-lg-9 col-sm-9 col-md-9 col-xs-9">  
-    	<h1>   
-     		<? the_title(); ?>     
-     	</h1>
-    	<? the_content(); ?>    
-     
-    </div>
-     
+    			</div>
+			</div>
+		</div>
      <?php get_sidebar(); ?>
  </div>
 
