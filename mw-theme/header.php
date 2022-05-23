52739c84fd79558b583205d9eda7973aa276796f
Arndik <104499187+Arndik@users.noreply.github.com>
2022-05-23 16:44:07 +0200

    Update header.php

--- mw-theme/header.php
+++ mw-theme/header.php
@@ -41,18 +41,6 @@
                             ?>
                                 <div class="navbar-brand">
                                     <img src=" <?php echo get_stylesheet_directory_uri() . '/img/logo.svg'; ?>" width=100%>
-                                    <div style=position:relative>
-                                        <div class=startanimation>
-                                            <div class="ensemblefahne kinder move anim">Kinderchor</div>
-                                            <div class="ensemblefahne jugend move anim">Jugendchor</div>
-                                            <div class="ensemblefahne pop move anim">Pop-Chor</div>
-                                            <div class="ensemblefahne offener move anim">Offener Chor</div>
-                                            <div class="ensemblefahne projekt move anim">Projektchöre</div>
-                                        </div>
-                                    </div>
-                                
-                                
-                                
                                 </div>
                             <?php
                             }	
