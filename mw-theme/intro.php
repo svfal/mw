7cda9d7523729b2f9254246e9a8cfb3c901ea7d6
Arndik <104499187+Arndik@users.noreply.github.com>
2022-05-23 17:26:52 +0200

    Update intro.php

--- mw-theme/intro.php
+++ mw-theme/intro.php
@@ -18,9 +18,9 @@
             <div class="ensemblefahne projekt move anim">Projektchöre</div>
         </div>
     </div>
-    <div class=container>
-    <h2>Magst du Cookies?</h2>
-    <p><input type=radio name=cookie value=yep>klar
-    <p><input type=radio name=cookie value=maybe>kommt drauf an
-    <p><input type=radio name=cookie value=nope>krpfrz!
-</div>
+    <div style=margin-left:10em>
+            <h2>Magst du Cookies?</h2>
+            <p><input type=radio name=cookie value=yep>klar
+            <p><input type=radio name=cookie value=maybe>kommt drauf an
+             <p><input type=radio name=cookie value=nope>krpfrz!
+    </div>
