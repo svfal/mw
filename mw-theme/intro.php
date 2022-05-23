dd17303eb542c3ff80c788f1bd63e0559ca812d8
Arndik <104499187+Arndik@users.noreply.github.com>
2022-05-23 16:59:30 +0200

    Update intro.php

--- mw-theme/intro.php
+++ mw-theme/intro.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Musikwerk Stuttgart</title>
-    <?php wp_head(); ?>
-     
+    
+    <link rel='stylesheet' href='style.css'>
 </head>
 <body>
     <div style=position:relative>
