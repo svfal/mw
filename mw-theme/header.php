846a93b03ee6d7cd10457f3ccb3f07dfc47690b3
Arndik <104499187+Arndik@users.noreply.github.com>
2022-05-24 18:02:32 +0200

    Update header.php

--- mw-theme/header.php
+++ mw-theme/header.php
@@ -104,7 +104,7 @@
                                         'container'         => 'div',
                                         'container_class'   => 'collapse navbar-collapse ',
                                         'container_id'      => 'bs-example-navbar-collapse-1',
-                                        'menu_class'        => 'nav navbar-nav flex-column flex-sm-row offset-md-3 offset-lg-0 flex-sm-nowrap px-0 pt-3',
+                                        'menu_class'        => 'nav navbar-nav flex-column flex-sm-row offset-md-3 offset-lg-0 flex-sm-nowrap px-0 ',
                                         'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                                         'walker'            => new WP_Bootstrap_Navwalker(),
                                         ));
