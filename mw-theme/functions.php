f7f673be9059116b6897d1d6e734afe6dbae94b8
DanielOliver89 <Daniel.Kimmich@gmx.de>
2022-07-24 14:18:55 +0200

    Update functions.php
    Mehr Einstellungsmöglichkeiten für Woocomerce in functions.php

--- mw-theme/functions.php
+++ mw-theme/functions.php
@@ -61,7 +61,20 @@ function prefix_bs5_dropdown_data_attribute( $atts, $item, $args ) {
  * Woocommerce
  */
 function mw_theme_add_woocommerce_support() {
-	add_theme_support( 'woocommerce' );
+	add_theme_support( 'woocommerce', array(
+		'thumbnail_image_width' => 150,
+		'single_image_width'    => 300,
+
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+	) );
 }
+
 add_action( 'after_setup_theme', 'mw_theme_add_woocommerce_support' );
 ?>
