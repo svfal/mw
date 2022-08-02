4485a4916b7f9d7617fccec6132aabf25cae17c2
Arndik <104499187+Arndik@users.noreply.github.com>
2022-08-02 10:42:49 +0200

    Update page.php

--- mw-theme/page.php
+++ mw-theme/page.php
@@ -2,7 +2,7 @@
  	<div id="content" >  
 		<div class="container">
 			<div class="row justify-content-center">
-				<div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 mt-1">	  	
+				<div class="col-lg-10 col-md-12 col-sm-12 col-xs-12">	  	
      				    <h1> 
      						<? the_title(); ?>     
      					</h1>
