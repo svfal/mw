bdc4d00f7bc47f588cfa03445301f2145459e081
Arndik <104499187+Arndik@users.noreply.github.com>
2022-08-02 10:41:24 +0200

    Update page.php

--- mw-theme/page.php
+++ mw-theme/page.php
@@ -6,12 +6,14 @@
      				    <h1> 
      						<? the_title(); ?>     
      					</h1>
-    					<? the_content(); ?>    
+						<div class=container>
+    						<? the_content(); ?>   
+						</div> 
      
     			</div>
 			</div>
 		</div>
      
- </div>
+    </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
