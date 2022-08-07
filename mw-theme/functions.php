257bfb993182945f2dc010e57b5c5e4fdc52088d
Arndik <104499187+Arndik@users.noreply.github.com>
2022-08-07 11:47:28 +0200

    Update functions.php

--- mw-theme/functions.php
+++ mw-theme/functions.php
@@ -9,6 +9,30 @@ function musikwerk_files(){
     wp_enqueue_style('bootstrap_style', get_stylesheet_directory_uri() . '/style.css');
     
 }
+function load_theme_textdomain( $domain, $path = false ) {
+    /**
+     * Filters a theme's locale.
+     *
+     * @since 3.0.0
+     *
+     * @param string $locale The theme's current locale.
+     * @param string $domain Text domain. Unique identifier for retrieving translated strings.
+     */
+    $locale = apply_filters( 'theme_locale', determine_locale(), $domain );
+ 
+    $mofile = $domain . '-' . $locale . '.mo';
+ 
+    // Try to load from the languages directory first.
+    if ( load_textdomain( $domain, WP_LANG_DIR . '/themes/' . $mofile ) ) {
+        return true;
+    }
+ 
+    if ( ! $path ) {
+        $path = get_template_directory();
+    }
+ 
+    return load_textdomain( $domain, $path . '/' . $locale . '.mo' );
+}
 add_action('wp_enqueue_scripts', 'musikwerk_files');
 
  
