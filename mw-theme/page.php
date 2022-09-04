f040a5a15dd18bbaf41e3d09725ad5626e98a8e3
Arndik <104499187+Arndik@users.noreply.github.com>
2022-09-04 21:38:45 +0200

    Füge id der Seitentitel ein, um sie mit css ansteuern zu können.

--- mw-theme/page.php
+++ mw-theme/page.php
@@ -3,9 +3,12 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-lg-10 col-md-12 col-sm-12 col-xs-12">	  	
-     				    <h1> 
-     						<? the_title(); ?>     
-     					</h1>
+					<h1>
+                		<span id=<? echo "title".get_the_ID()?>> 
+     							<? the_title(); ?>   
+                                
+                        </span>  
+     				</h1>
 						
     						<? the_content(); ?>   
 						
