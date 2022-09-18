5c7599c690d541eb135895f65028eb3dd67f17e9
DanielOliver89 <Daniel.Kimmich@gmx.de>
2022-09-18 16:05:07 +0200

    Update functions.php

--- mw-theme/functions.php
+++ mw-theme/functions.php
@@ -148,8 +148,8 @@ function woocommerce_header_add_to_cart_fragment($fragments)
 	ob_start();
 
 ?>
-	<a class=" cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count); ?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+	<a class="wcmenucart-contents" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count); ?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
 <?php
-	$fragments['a.cart-customlocation'] = ob_get_clean();
+	$fragments['a.wcmenucart-contents'] = ob_get_clean();
 	return $fragments;
 }
