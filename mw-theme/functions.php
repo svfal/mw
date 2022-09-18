18fe16f7bd59bf16214774c39f9ec4347a60299a
DanielOliver89 <Daniel.Kimmich@gmx.de>
2022-09-18 16:38:41 +0200

    Update functions.php

--- mw-theme/functions.php
+++ mw-theme/functions.php
@@ -136,13 +136,20 @@ function sk_wcmenucart($menu, $args) {
 
 }
 
-add_filter( 'woocommerce_add_to_cart_fragments', 'wc_refresh_mini_cart_count');
-function wc_refresh_mini_cart_count($fragments){
-    ob_start();
-    $items_count = WC()->cart->get_cart_contents_count();
-    ?>
-    <div id="wcmenucart-contents"><?php echo $items_count ? $items_count : '&nbsp;'; ?></div>
-    <?php
-        $fragments['.wcmenucart-contents'] = ob_get_clean();
-    return $fragments;
+/**
+ * Show cart contents / total Ajax
+ */
+add_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+
+function woocommerce_header_add_to_cart_fragment($fragments)
+{
+	global $woocommerce;
+
+	ob_start();
+
+?>
+	<a class="wcmenucart-contents" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count); ?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+<?php
+	$fragments['a.wcmenucart-contents'] = ob_get_clean();
+	return $fragments;
 }
