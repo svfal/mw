9ccc3e201886043c2c788ffb2b74ca55485016aa
fyayc-dki <dki@foryouandyourcustomers.com>
2022-10-01 17:45:09 +0200

    Update content-single-product.php

--- mw-theme/woocommerce/content-single-product.php
+++ mw-theme/woocommerce/content-single-product.php
@@ -33,7 +33,7 @@ if ( post_password_required() ) {
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 	<div class="container">
-	<div class="row">
+	<div class="row justify-content-center">
 		<div class="col-4">
 			<?php
 				/**
