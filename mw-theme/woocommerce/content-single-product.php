29141f5794ecbf42c47368362d520e5ac6dec3ae
fyayc-dki <dki@foryouandyourcustomers.com>
2022-10-01 13:14:12 +0200

    Update content-single-product.php

--- mw-theme/woocommerce/content-single-product.php
+++ mw-theme/woocommerce/content-single-product.php
@@ -32,21 +32,20 @@ if ( post_password_required() ) {
 }
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
-
-<div class="container">
+	<div class="container">
 	<div class="row">
-		<div class="col-4">
+		<div class="col">
 			<?php
-			/**
-			 * Hook: woocommerce_before_single_product_summary.
-			 *
-			 * @hooked woocommerce_show_product_sale_flash - 10
-			 * @hooked woocommerce_show_product_images - 20
-			 */
-			do_action( 'woocommerce_before_single_product_summary' );
+				/**
+				 * Hook: woocommerce_before_single_product_summary.
+				 *
+				 * @hooked woocommerce_show_product_sale_flash - 10
+				 * @hooked woocommerce_show_product_images - 20
+				 */
+				do_action( 'woocommerce_before_single_product_summary' );
 			?>
 		</div>
-		<div class="col-4"></div>
+		<div class="col">
 			<div class="summary entry-summary">
 				<?php
 				/**
@@ -66,18 +65,23 @@ if ( post_password_required() ) {
 			</div>
 
 			<?php
-			/**
-			 * Hook: woocommerce_after_single_product_summary.
-			 *
-			 * @hooked woocommerce_output_product_data_tabs - 10
-			 * @hooked woocommerce_upsell_display - 15
-			 * @hooked woocommerce_output_related_products - 20
-			 */
-			do_action( 'woocommerce_after_single_product_summary' );
+				/**
+				 * Hook: woocommerce_after_single_product_summary.
+				 *
+				 * @hooked woocommerce_output_product_data_tabs - 10
+				 * @hooked woocommerce_upsell_display - 15
+				 * @hooked woocommerce_output_related_products - 20
+				 */
+				do_action( 'woocommerce_after_single_product_summary' );
 			?>
+	</div>
+		</div>
+		<div class="col">
+		
 		</div>
 	</div>
-</div>
+	</div>
+
 	
 
 	
