2573098c0c9c74908205f3a5daa143056efb34a7
fyayc-dki <dki@foryouandyourcustomers.com>
2022-10-20 22:20:25 +0200

    Update footer.php
    Impressum durch Interner Bereich ersetzen

--- mw-theme/footer.php
+++ mw-theme/footer.php
@@ -8,7 +8,7 @@
                 
                      <div><a href="/" class=footerlink>Home</a></div>
                     <div><a href=datenschutz class=footerlink>Datenschutz</a></div>
-                    <div><a href=impressum class=footerlink>Impressum</a></div>
+                    <div><a href="https://intern.musikwerk-stuttgart.de" class=footerlink>Interner Bereich</a></div>
                     <div><a href=kontakt class=footerlink>Kontakt</a></div>
                     
                     <div ><a href=https://www.facebook.com/musikwerk.stuttgart target=_NEW><img src= <?php echo get_stylesheet_directory_uri() . '/img/fb4.png' ?> width=35 ></a></div>
@@ -20,7 +20,7 @@
             <div class="d-flex d-sm-none flex-row col-12 justify-content-evenly align-items-center py-2" style= 'border-top: #16a085 2px solid;'>
                     <div><a href="/" class=footerlink>Home</a></div>
                     <div><a href=datenschutz class=footerlink>Datenschutz</a></div>
-                    <div><a href=impressum class=footerlink>Impressum</a></div>
+                    <div><a href="https://intern.musikwerk-stuttgart.de" class=footerlink>Interner Bereich</a></div>
                     <div><a href=kontakt class=footerlink>Kontakt</a></div>
             </div>
             <div class="d-flex d-sm-none flex-row col-12 justify-content-center align-items-center" >       
